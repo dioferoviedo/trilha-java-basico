4c166ea37c47a40a59a098b07d85154cd422ddc3
Diofer Oviedo <dioferteonilo@gmail.com>
2023-04-22 21:26:48 -0300

    Att

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá mundo");
+        //tipos primitivos
+        //classe String representa texto na aplicação
+
+        String meuNome = "Diofer Oviedo";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
